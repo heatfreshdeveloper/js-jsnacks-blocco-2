cc327fa8fa02ef8382350040b8edaaf2ab4f7131
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-10-15 17:04:56 +0200

    esercizio array completato

---
+++ array.js
@@ -0,0 +1,10 @@
+const array1 = [15, 2, 7];
+const array2 = [5];
+
+let numeroMassimo;
+
+// creiamo un ciclo che inserisca valori nell'array 2 come quelli dell'array 1
+for (i = 0; array1.length === array2.length; i++ ) {
+
+    numeroMassimo = prompt(`Inserisci un valore; Hai disposizione ${i} valori`);
+}
